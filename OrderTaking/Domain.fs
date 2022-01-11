030f1197c14bd7229af38d5b338772d47c9bf61a
jhondev <jma9974@gmail.com>
2022-01-10 22:08:54 -0500

    implementing pipeline wip1

--- OrderTaking/Domain.fs
+++ OrderTaking/Domain.fs
@@ -1,48 +1,81 @@
 namespace OrderTaking.Domain
 
-// Value Objects
-type WidgetCode = WidgetCode of string // W = 4 digits
-type GizmoCode = GizmoCode of string // G = 3 digits
+module Domain = 
+    // Value Objects
+    type WidgetCode = WidgetCode of string // W = 4 digits
+    type GizmoCode = GizmoCode of string // G = 3 digits
 
-type ProductCode =
-    | Widget of WidgetCode
-    | Gizmo of GizmoCode
+    type ProductCode =
+        | Widget of WidgetCode
+        | Gizmo of GizmoCode
 
-type UnitQuantity = UnitQuantity of int
-type KilogramQuantity = KilogramQuantity of decimal
+    type UnitQuantity = UnitQuantity of int
+    type KilogramQuantity = KilogramQuantity of decimal
 
-type OrderQuantity =
-    | Unit of UnitQuantity
-    | Kilos of KilogramQuantity
+    type OrderQuantity =
+        | Unit of UnitQuantity
+        | Kilos of KilogramQuantity
 
-// Entities
-type OrderId = Undefined
-type OrderLineId = Undefined
-type CustomerId = Undefined
+    // Entities
+    type OrderId = private OrderId of string
+        
+    // module OrderId =
+    //     /// Define a "Smart constructor" for OrderId​
+    //     /// string -> OrderId​
+    //     let create str = 
+    //         if String.IsNullOrEmpty(str) then
+    //             // use exceptions rather than Result for now​
+    //             failwith "OrderId must not be null or empty"
+    //         elif str.Length > 50 then
+    //             failwith "OrderId must not be more than 50 chars"
+    //         else
+    //             OrderId str
+                
+    //     let value (OrderId str) = str
 
-type CustomerInfo = Undefined
-type ShippingAddress = Undefined
-type BillingAddress = Undefined
-type Price = Undefined
-type BillingAmount = Undefined
+    type OrderLineId = Undefined
+    type CustomerId = Undefined
 
-type Order =
-    { Id: OrderId
-      CustomerId: CustomerId
-      ShippingAddress: ShippingAddress
-      BillingAddress: BillingAddress
-      OrderLines: OrderLine list
-      AmountToBill: BillingAmount }
+    type CustomerInfo = Undefined
+    type ShippingAddress = Undefined
+    type BillingAddress = Undefined
+    type Price = Undefined
+    type BillingAmount = Undefined
 
-and OrderLine =
-    { Id: OrderLineId
-      OrderId: OrderId
-      ProductCode: ProductCode
-      OrderQuantity: OrderQuantity
-      Price: Price }
+    type Order = { 
+        Id: OrderId
+        CustomerId: CustomerId
+        ShippingAddress: ShippingAddress
+        BillingAddress: BillingAddress
+        OrderLines: OrderLine list
+        AmountToBill: BillingAmount 
+    }
 
-// type UnvalidatedOrder = {
-//     OrderId: string
-//     CustomerInfo: string
-//     ShippingAddress: string
-// }
+    and OrderLine = { 
+        Id: OrderLineId
+        OrderId: OrderId
+        ProductCode: ProductCode
+        OrderQuantity: OrderQuantity
+        Price: Price 
+    }
+
+    type UnvalidatedOrder = {
+        OrderId: string
+        CustomerInfo: string
+        ShippingAddress: string
+    }
+
+    // type CheckAddressExists = UnvalidatedAddress -> AsyncResult<CheckedAddress,AddressValidationError>
+    // type ValidateOrder =
+    //   CheckProductCodeExists    // dependency​
+    //     -> CheckAddressExists   // AsyncResult dependency​
+    //     -> UnvalidatedOrder     // input​
+    //     -> AsyncResult<ValidatedOrder,ValidationError list>  // output​
+    
+    // type CheckAddressExists = UnvalidatedAddress -> CheckedAddress
+    // type ValidateOrder =
+    //   CheckProductCodeExists    // dependency​
+    //     -> CheckAddressExists   // AsyncResult dependency​
+    //     -> UnvalidatedOrder     // input​
+    //     -> ValidatedOrder       // output​
+
